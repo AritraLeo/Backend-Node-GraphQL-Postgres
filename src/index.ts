aba9f60aea6c99c5f6182f4ba170909c1d0a97bb
AritraLeo <aritrabinance056@gmail.com>
2024-09-12 22:25:01 +0530

    Setup migrations for Users (Prisma) & Resolvers (Gql)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
+import { prismaClient } from './lib/db';
 
 const PORT = Number(process.env.PORT) || 8000;
 
@@ -13,10 +14,30 @@ async function init() {
             type Query {
                 hello: String
             }
+
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => `Hey Test GQL server!`
+            },
+
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }:
+                    { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            email,
+                            firstName,
+                            lastName,
+                            password,
+                            salt: '12'
+                        }
+                    });
+                    return true;
+                }
             }
         }
     });
